702747d2a45de68cda153f038359dec020bc33c1
xiaogit00 <leibingguo@gmail.com>
2022-11-16 22:08:56 +0800

    finished transactions component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import TokenBalances from './components/TokenBalances'
 import ERC20Transfers from './components/ERC20Transfers'
 import TokenHolders from './components/TokenHolders'
-import Transactions from './components/TokenHolders'
+import Transactions from './components/Transactions'
 
 export {
 	TokenBalances,
