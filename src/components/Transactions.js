89197aa5a1af3e5c662be876084da47818e0e996
xiaogit00 <leibingguo@gmail.com>
2022-11-21 18:23:55 +0800

    Editted in fetchData, refactored blockExplorerURL, and tab spacing issue

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,64 +1,45 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { transform } from '../utils/transform';
-import { categorizeTransaction } from '../utils/categorize';
-import 'antd/dist/antd.css';
-import { Table} from 'antd';
-import { blockExplorerURLs } from '../utils/blockExplorerURLs';
+import React, { useState, useEffect } from 'react'
+import { transform } from '../utils/transform'
+import { categorizeTransaction } from '../utils/categorize'
+import 'antd/dist/antd.css'
+import { Table } from 'antd'
+import { blockExplorerURLs } from '../utils/blockExplorerURLs'
 import { columns } from '../utils/columns'
+import { getDataFromCovalentAPI } from '../utils/api'
 
+const Transactions = ({ chainId, address }) => {
+  const [txns, setTxns] = useState(undefined)
+  const [isLoading, setIsLoading] = useState(true)
 
+  const blockexplorerURL = blockExplorerURLs.filter(
+    (item) => parseInt(item.chainId) === parseInt(chainId)
+  )[0].url
+  const transactionsEndpoint = `https://api.covalenthq.com/v1/${chainId}/address/${address}/transactions_v2/`
 
-const Transactions = ( {chainId, address}) => {
-    const [txns, setTxns] = useState(undefined);
-    const [isLoading, setIsLoading] = useState(true)
-    const apiKey = process.env.REACT_APP_COVALENT_API_KEY
+  useEffect(() => {
+    getDataFromCovalentAPI(transactionsEndpoint)
+      .then((res) => {
+        const transformedTransactions = transform(
+          res.data.items.filter((txn) => txn.log_events.length < 20)
+        ) //remove spam
 
-    const blockexplorerURL = blockExplorerURLs.filter(item => parseInt(item.chainId) === parseInt(chainId))[0].url
-    const transactionsEndpoint = `https://api.covalenthq.com/v1/${chainId}/address/${address}/transactions_v2/`;
-    const auth = {
-        username: apiKey,
-        password: ''
-    }
+        const categorizedTransactions = transformedTransactions.map((txn) => {
+          return categorizeTransaction(txn, address)
+        })
 
-    const options = {
-        url: transactionsEndpoint,
-        method: "GET",
-        auth: auth
-    };
+        setIsLoading(false)
+        setTxns(categorizedTransactions)
+      })
+      .catch((err) => console.log(err.message))
+  }, [address])
 
-    useEffect(() => {
-        axios(options)
-            .then((res) => {
-
-                const transformedTransactions = transform(res.data.data.items.filter(txn => txn.log_events.length < 20)) //remove spam
-
-                const categorizedTransactions = transformedTransactions.map((txn) => {
-                    return categorizeTransaction(txn, address);
-                })
-
-                setIsLoading(false)
-                setTxns(categorizedTransactions)
-                
-            })
-            .catch( (err) => console.log(err.message))
-    }, [address])
-
-    if (isLoading) {
-        return (
-            <Table loading={true} />
-        )
-    } else if (!isLoading && txns) {
-        console.log("txnData",txns)
-        return (
-            <Table dataSource={txns} columns={columns(blockexplorerURL)} rowKey='txnHash'/>
-        )
-    } else {
-        return null
-    }
+  if (isLoading) {
+    return <Table loading={true} />
+  } else if (!isLoading && txns) {
+    return <Table dataSource={txns} columns={columns(blockexplorerURL)} rowKey="txnHash" />
+  } else {
+    return null
+  }
 }
 
 export default Transactions
-
-
-
--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ERC20Transfers from './components/ERC20Transfers'
 import TokenHolders from './components/TokenHolders'
 import Transactions from './components/Transactions'
 
-export {
-	TokenBalances,
-	ERC20Transfers,
-	TokenHolders,
-	Transactions
-}
+export { TokenBalances, ERC20Transfers, TokenHolders, Transactions }
